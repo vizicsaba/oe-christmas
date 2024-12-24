b683233e845b95d058346d24d07b551146538b49
vizicsaba <64264159+vizicsaba@users.noreply.github.com>
2024-12-24 21:37:46 +0100

    Update hundredtest.c

--- hundredtest.c
+++ hundredtest.c
@@ -1,1 +1,15 @@
-#oe-christmas
+#include <stdio.h>
+
+int main() {
+    int sum = 0;
+
+    // Összeadás ciklussal
+    for (int i = 1; i <= 100; i++) {
+        sum += i;
+    }
+
+    // Eredmény kiírása
+    printf("Az 1-től 100-ig terjedő számok összege: %d\n", sum);
+
+    return 0;
+}
